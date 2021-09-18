f91dabe38ff02ae12b2ea1f171670a5d4d2d4652
Gemasss <58686523+Gemafauzi@users.noreply.github.com>
2021-09-18 10:27:02 +0700

    sudah selesai menonton vidio dan mengejarkan

---
+++ constructor.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+class Produk{
+    public  $judul  = "judul", 
+            $penulis =  "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
+
+    public function __construct($judul, $penulis, $penerbit, $harga) {
+       $this->judul= $judul;
+       $this->penulis= $penulis;
+       $this->penerbit= $penerbit;
+       $this->harga= $harga;
+
+    }
+        
+    
+            
+        public function getLabel() {
+
+        return "$this->penulis, $this->penerbit";
+           }
+     
+    
+    }       
+
+
+$produk1 =  new Produk("Naruto","Masahi Kishimoto","Shonen Jump", 30000) ;
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
